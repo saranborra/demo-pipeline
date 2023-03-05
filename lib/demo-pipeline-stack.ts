374903186245bdce21ebdb537dd20d41264c5b5c
saran <saranjune20@gmail.com>
2023-03-05 08:06:16 +0530

    added lambda

--- lib/demo-pipeline-stack.ts
+++ lib/demo-pipeline-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import {Stack,StackProps} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
-import { DemoPipelineAppStage } from "./demo-app-stage";
+// import { DemoPipelineAppStage } from "./demo-app-stage";
 
 export class DemoPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -10,17 +10,11 @@ export class DemoPipelineStack extends Stack {
 
     const pipeline = new CodePipeline(this, "Pipeline", {
         pipelineName: "DemoPipeline",
-        crossAccountKeys: true, //multi-account deployment
+        // crossAccountKeys: true, //multi-account deployment
         synth: new ShellStep("Synth", {
             input: CodePipelineSource.gitHub("saranborra/demo-pipeline", "main"),
             commands: ["npm ci", "npm run build", "npx cdk synth"],
         }),
     });
-
-    pipeline.addStage(
-        newDemoPipelineAppStage(this, "test",{
-            env:{account:"096721594425", region: "us-east-2"},
-        })
-    );
     }
 }
