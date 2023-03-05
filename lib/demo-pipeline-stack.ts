4bce0c3cc1d39f4665d27e8b8d1999514021d629
saran <saranjune20@gmail.com>
2023-03-05 07:36:13 +0530

    added lambda

--- lib/demo-pipeline-stack.ts
+++ lib/demo-pipeline-stack.ts
@@ -6,15 +6,17 @@ import { DemoPipelineAppStage } from "./demo-app-stage";
 export class DemoPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
     const pipeline = new CodePipeline(this, "Pipeline", {
         pipelineName: "DemoPipeline",
         crossAccountKeys: true, //multi-account deployment
         synth: new ShellStep("Synth", {
             input: CodePipelineSource.gitHub("saranborra/demo-pipeline", "main"),
-            installCommands: ['npm i -g npm@latest'],
+            //installCommands: ['npm i -g npm@latest'],
             commands: ["npm ci", "npm run build", "npx cdk synth"],
         }),
     });
+
     pipeline.addStage(
         newDemoPipelineAppStage(this, "test",{
             env:{account:"096721594425", region: "us-east-2"},
