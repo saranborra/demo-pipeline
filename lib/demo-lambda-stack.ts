0af630d2a5fabab09d937c8798310293ec6905c0
saran <saranjune20@gmail.com>
2023-03-05 08:30:51 +0530

    added lambda

--- lib/demo-lambda-stack.ts
+++ lib/demo-lambda-stack.ts
@@ -1,10 +1,10 @@
 import {Stack, StackProps} from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { Function, Code, Runtime } from "aws-cdk-lib/aws-lambda";
-import { LambdaRestApi } from "aws-cdk-lib/aus-apigateway";
+import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
 
 export class DemoLambdaStack extends Stack{
-	constructor(scope: Construct, id: string, props?: StackPraps) {
+	constructor(scope: Construct, id: string, props?: StackProps) {
 		super(scope, id, props);
 
 		const index = new Function (this, "LambdaFunction", {
